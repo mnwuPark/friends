ae74db06972fc36c161d2978b0aa6dd6d9728c66
mnwuPark <mn.Park@geeksbin.com>
2022-09-15 12:57:38 +0900

    Kong

--- Kong.py
+++ Kong.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from discord.utils import get_slots
 from discord import FFmpegPCMAudio
 
-token = "**"
+token = ""
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -46,7 +46,7 @@ async def say(ctx, *, text):
    await ctx.send(embed = discord.Embed(title = "Say", description = text, color = 0xffffff))
 
 @bot.command()
-async def URL재생(ctx, *, url):
+async def URLplay(ctx, *, url):
     YDL_OPTIONS = {'format': 'bestaudio','noplaylist':'True'}
     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
@@ -58,5 +58,31 @@ async def URL재생(ctx, *, url):
         await ctx.send(embed = discord.Embed(title= "Play Music", description = "Now played Music: " + url, color = 0x00ff00))
     else:
         await ctx.send("Music has been played now!")
+
+@bot.command()
+async def play(ctx, *, msg):
+    if not vc.is_playing():
+        global entireText
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            
+        chromedriver_dir = r"C:\code\py\Kong\chromedriver.exe"
+        driver = webdriver.Chrome(chromedriver_dir)
+        driver.get("https://www.youtube.com/results?search_query="+msg+"+lyrics")
+        source = driver.page_source
+        bs = bs4.BeautifulSoup(source, 'lxml')
+        entire = bs.find_all('a', {'id': 'video-title'})
+        entireNum = entire[0]
+        entireText = entireNum.text.strip()
+        musicurl = entireNum.get('href')
+        url = 'https://www.youtube.com'+ musicurl 
+
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        await ctx.send(embed = discord.Embed(title= "Play Music", description = "Now played Music: " + entireText, color = 0x00ff00))
+        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+    else:
+        await ctx.send(embed = discord.Embed(title = "Music", description = "Can't play the Music: Just played Music now :!", color = 0xff0000))
 
 bot.run(token)
