f66c68d7dfb24048c80b1988a11a7c117d02b6a1
mnwuPark <mn.Park@geeksbin.com>
2022-09-15 11:40:21 +0900

    Kong

--- Kong.py
+++ Kong.py
@@ -13,3 +13,16 @@ async def on_ready():
     print("Login to: ", bot.user.name)
     print("connection was succesful")
     await bot.change_presence(status = discord.Status.online, activity = None)
+
+@bot.command()
+async def join(ctx):
+    try:
+        global vc
+        vc = await ctx.message.author.voice.channel.connect()
+        await ctx.send(embed = discord.Embed(title = "Kong", description = "Join to channel..", color = 0x000000))
+    except:
+        try:
+            await vc.move_to(ctx.message.author.voice.channel)
+        except:
+            await ctx.send(embed = discord.Embed(title = "Kong", description = "You don't join some channel", color = 0x000000))
+
