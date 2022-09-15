222382d23d64711a5d69a38c4e466cb3588f661c
yhEom <113492012+yhEom@users.noreply.github.com>
2022-09-15 17:53:49 +0900

    뭉탱이
    유링게슝

--- mdp.c
+++ mdp.c
@@ -1,74 +1,74 @@
-#include <mega128a.h> //ATmega128a ÇÁ·Î¼¼¼­ Çì´õÆÄÀÏ
-#include <delay.h> //µô·¹ÀÌ Çì´õÆÄÀÏ
-#include <lcd.h> //LCD Á¦¾î ¶óÀÌºê·¯¸® Çì´õÆÄÀÏ
+#include <mega128a.h> //ATmega128a í”„ë¡œì„¸ì„œ í—¤ë”íŒŒì¼
+#include <delay.h> //ë”œë ˆì´ í—¤ë”íŒŒì¼
+#include <lcd.h> //LCD ì œì–´ ë¼ì´ë¸ŒëŸ¬ë¦¬ í—¤ë”íŒŒì¼
 
 /*
-asm : ¾î¼Àºí¸® ¾ð¾î ±¸°£ ½ÃÀÛ
-.equ __lcd_port 0x15; : ¾î¼Àºí¸® ¾ð¾î·Î ¶óÀÌºê·¯¸® ³» º¯¼ö º¯°æ
-endasm : ¾î¼Àºí¸® ¾ð¾î ±¸°£ ³¡
+asm : ì–´ì…ˆë¸”ë¦¬ ì–¸ì–´ êµ¬ê°„ ì‹œìž‘
+.equ __lcd_port 0x15; : ì–´ì…ˆë¸”ë¦¬ ì–¸ì–´ë¡œ ë¼ì´ë¸ŒëŸ¬ë¦¬ ë‚´ ë³€ìˆ˜ ë³€ê²½
+endasm : ì–´ì…ˆë¸”ë¦¬ ì–¸ì–´ êµ¬ê°„ ë
 */ 
 #asm
     .equ __lcd_port = 0x15;
 #endasm
 
-bit sw_toggle = 0; //bit Å©±â·Î µ¥ÀÌÅÍ Á¦¾î 0,1
+bit sw_toggle = 0; //bit í¬ê¸°ë¡œ ë°ì´í„° ì œì–´ 0,1
 
-void Init(); //ÀÌ´Ï¼È¶óÀÌÆÃ ÇÔ¼ö ¼±¾ð
-void Control(); //ÄÁÆ®·Ñ ÇÔ¼ö ¼±¾ð
+void Init(); //ì´ë‹ˆì…œë¼ì´íŒ… í•¨ìˆ˜ ì„ ì–¸
+void Control(); //ì»¨íŠ¸ë¡¤ í•¨ìˆ˜ ì„ ì–¸
 
-void main(void) //¸ÞÀÎ ÇÔ¼ö ½ÃÀÛ
+void main(void) //ë©”ì¸ í•¨ìˆ˜ ì‹œìž‘
 {
-    Init(); //ÀÌ´Ï¼È¶óÀÌÆÃ ÇÔ¼ö È£Ãâ
-    while(1) //¹«ÇÑ ¹Ýº¹
+    Init(); //ì´ë‹ˆì…œë¼ì´íŒ… í•¨ìˆ˜ í˜¸ì¶œ
+    while(1) //ë¬´í•œ ë°˜ë³µ
     {
-        Control(); //ÄÁÆ®·Ñ ÇÔ¼ö È£Ãâ      
+        Control(); //ì»¨íŠ¸ë¡¤ í•¨ìˆ˜ í˜¸ì¶œ      
     }
 }
 
-void Init() //ÀÌ´Ï¼È¶óÀÌÆÃ ÇÔ¼ö ½ÃÀÛ
+void Init() //ì´ë‹ˆì…œë¼ì´íŒ… í•¨ìˆ˜ ì‹œìž‘
 {
-    DDRA = 0xff; //PORTAÀÇ ¹æÇâ ¼³Á¤ (Ãâ·Â)
-    DDRC = 0xff; //PORTCÀÇ ¹æÇâ ¼³Á¤ (Ãâ·Â)
-    DDRE = 0x00; //PORTEÀÇ ¹æÇâ ¼³Á¤ (ÀÔ·Â)
+    DDRA = 0xff; //PORTAì˜ ë°©í–¥ ì„¤ì • (ì¶œë ¥)
+    DDRC = 0xff; //PORTCì˜ ë°©í–¥ ì„¤ì • (ì¶œë ¥)
+    DDRE = 0x00; //PORTEì˜ ë°©í–¥ ì„¤ì • (ìž…ë ¥)
     
-    PORTA = 0x00; //PORTAÀÇ Ãâ·Â ¼³Á¤
-    PORTC = 0xff; //PORTCÀÇ Ãâ·Â ¼³Á¤
-    PORTE = 0x00; //PORTEÀÇ Ãâ·Â ¼³Á¤¤
+    PORTA = 0x00; //PORTAì˜ ì¶œë ¥ ì„¤ì •
+    PORTC = 0xff; //PORTCì˜ ì¶œë ¥ ì„¤ì •
+    PORTE = 0x00; //PORTEì˜ ì¶œë ¥ ì„¤ì •
     
-    EIMSK = 0x80; //¿ÜºÎÀÎÅÍ·´Æ®¸¶½ºÅ©¼³Á¤ 0b10000000 INT7¸¸ È°¼ºÈ­
-    EICRA = 0x00; //¿ÜºÎÀÎÅÍ·´Æ®·¹Áö½ºÅÍA 0b00000000 
-    EICRB = 0x80; //¿ÜºÎÀÎÅÍ·´Æ®·¹Áö½ºÅÍB 0b10000000 ¿ÜºÎÀÎÅÍ·´Æ® Æ®¸®°Å ¼³Á¤ ÀÚ¼¼ÇÑ ¼³¸í https://wowon.tistory.com/7
-    SREG = 0x80; //·¹Áö½ºÅÍ »óÅÂ Á¦¾î º¯¼ö ÀÎÅÍ·´Æ® »ç¿ë½Ã ¹«Á¶°Ç 0x80
+    EIMSK = 0x80; //ì™¸ë¶€ì¸í„°ëŸ½íŠ¸ë§ˆìŠ¤í¬ì„¤ì • 0b10000000 INT7ë§Œ í™œì„±í™”
+    EICRA = 0x00; //ì™¸ë¶€ì¸í„°ëŸ½íŠ¸ë ˆì§€ìŠ¤í„°A 0b00000000 
+    EICRB = 0x80; //ì™¸ë¶€ì¸í„°ëŸ½íŠ¸ë ˆì§€ìŠ¤í„°B 0b10000000 ì™¸ë¶€ì¸í„°ëŸ½íŠ¸ íŠ¸ë¦¬ê±° ì„¤ì • ìžì„¸í•œ ì„¤ëª… https://wowon.tistory.com/7
+    SREG = 0x80; //ë ˆì§€ìŠ¤í„° ìƒíƒœ ì œì–´ ë³€ìˆ˜ ì¸í„°ëŸ½íŠ¸ ì‚¬ìš©ì‹œ ë¬´ì¡°ê±´ 0x80
     
-    lcd_init(16); //LCD ±æÀÌ ¼³Á¤
-    lcd_gotoxy(0,0); //LCDÀÇ 0.0 ¹øÂ° È­¼Ò·Î ÀÌµ¿             
-    lcd_putsf(" IEMHS - MDP_01 "); //LCD Ç¥½Ã
-    lcd_gotoxy(0,1); //LCDÀÇ 0.1 ¹øÂ° È­¼Ò·Î ÀÌµ¿
-    lcd_putsf(" TEAM : CURTAIN "); //LCD Ç¥½Ã 
+    lcd_init(16); //LCD ê¸¸ì´ ì„¤ì •
+    lcd_gotoxy(0,0); //LCDì˜ 0.0 ë²ˆì§¸ í™”ì†Œë¡œ ì´ë™             
+    lcd_putsf(" IEMHS - MDP_01 "); //LCD í‘œì‹œ
+    lcd_gotoxy(0,1); //LCDì˜ 0.1 ë²ˆì§¸ í™”ì†Œë¡œ ì´ë™
+    lcd_putsf(" TEAM : CURTAIN "); //LCD í‘œì‹œ 
     delay_ms(1000); //1sec delay         
 }
 
-void Control()//ÄÁÆ®·Ñ ÇÔ¼ö ½ÃÀÛ
+void Control()//ì»¨íŠ¸ë¡¤ í•¨ìˆ˜ ì‹œìž‘
 {
-    if (sw_toggle == 0) //½ºÀ§Ä¡ Åä±Û È®ÀÎâ
+    if (sw_toggle == 0) //ìŠ¤ìœ„ì¹˜ í† ê¸€ í™•ì¸
     {                                 
-        PORTA = 0x01; //LED Á¡µî
-        lcd_gotoxy(0,0); //LCDÀÇ 0.0 ¹øÂ° È­¼Ò·Î ÀÌµ¿             
-        lcd_putsf(" CURTAIN  STATE "); //LCD Ç¥½Ã
-        lcd_gotoxy(0,1); //LCDÀÇ 0.1 ¹øÂ° È­¼Ò·Î ÀÌµ¿
-        lcd_putsf("       OFF      "); //LCD Ç¥½Ã   
+        PORTA = 0x01; //LED ì ë“±
+        lcd_gotoxy(0,0); //LCDì˜ 0.0 ë²ˆì§¸ í™”ì†Œë¡œ ì´ë™             
+        lcd_putsf(" CURTAIN  STATE "); //LCD í‘œì‹œ
+        lcd_gotoxy(0,1); //LCDì˜ 0.1 ë²ˆì§¸ í™”ì†Œë¡œ ì´ë™
+        lcd_putsf("       OFF      "); //LCD í‘œì‹œ   
     }
     else
     {              
-        PORTA = 0x02; //LED Á¡µî
-        lcd_gotoxy(0,0); //LCDÀÇ 0.0 ¹øÂ° È­¼Ò·Î ÀÌµ¿             
-        lcd_putsf(" CURTAIN  STATE "); //LCD Ç¥½Ã
-        lcd_gotoxy(0,1); //LCDÀÇ 0.1 ¹øÂ° È­¼Ò·Î ÀÌµ¿
-        lcd_putsf("       ON       "); //LCD Ç¥½Ã
+        PORTA = 0x02; //LED ì ë“±
+        lcd_gotoxy(0,0); //LCDì˜ 0.0 ë²ˆì§¸ í™”ì†Œë¡œ ì´ë™             
+        lcd_putsf(" CURTAIN  STATE "); //LCD í‘œì‹œ
+        lcd_gotoxy(0,1); //LCDì˜ 0.1 ë²ˆì§¸ í™”ì†Œë¡œ ì´ë™
+        lcd_putsf("       ON       "); //LCD í‘œì‹œ
     }
 }
 
-interrupt [EXT_INT7] void external_int7 (void) //¿ÜºÎ ÀÎÅÍ·´Æ® ÇÔ¼ö
+interrupt [EXT_INT7] void external_int7 (void) //ì™¸ë¶€ ì¸í„°ëŸ½íŠ¸ í•¨ìˆ˜
 {
-    sw_toggle = !sw_toggle; //sw_toggle ¹ÝÀü
+    sw_toggle = !sw_toggle; //sw_toggle ë°˜ì „
 }
