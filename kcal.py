19e2852f02bce923cbc26432f18b189f44a3fd39
mnwuPark <mw.park@geeksbin.com>
2022-09-16 10:09:27 +0900

    desc: 코드 간격 수정 cuz: 가독성 없어서

--- kcal.py
+++ kcal.py
@@ -1,31 +1,38 @@
 import os, datetime
+
 kcal = 0
 
 def wist():
     l = datetime.datetime.now()
-    ll = str(l.year)+"_"+str(l.month)+"_"+str(l.day)
+    ll = str(l.year) + "_" + str(l.month)+"_" + str(l.day)
     return ll
 
 def main():
     today = wist()
     file = os.getcwd()
-    if os.path.isfile(file+"\\"+today+".txt"):
-        opf = open(today+".txt", "r")
+
+    if os.path.isfile(file + "\\" + today + ".txt"):
+        opf = open(today + ".txt", "r")
+    
     else:
-        opf = open(today+".txt", "w")
+        opf = open(today + ".txt", "w")
         opf.write("0")
     opf.close()
+    
     while True:
         global kcal
         os.system('cls')
-        opf = open(today+".txt", "r")
+        
+        opf = open(today + ".txt", "r")
         opfr = opf.read()
         kcal = int(opfr)
         opf.close()
-        print("pre kcal = "+str(kcal))
+        
+        print("pre kcal = " + str(kcal))
         print("insert kcal\n")
+        
         kcal = int(input("")) + kcal
-        opf = open(today+".txt", "w")
+        opf = open(today + ".txt", "w")
         opf.write(str(kcal))
         opf.close()
 main()
